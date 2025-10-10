2d8606d61b4626d185601b5f00eddc05970d70ae
Divya Karade <72258493+DivyaKarade@users.noreply.github.com>
2025-10-10 23:08:37 +0530

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def show_dashboard():
+    st.header("Dashboard Module")
+    st.write("Modules under development:")
+    st.write("- Open Targets API integration")
+    st.write("- Compound similarity")
+    st.write("- Target prediction")
+    st.write("- Pathway mapper")
